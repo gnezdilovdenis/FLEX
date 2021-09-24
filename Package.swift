077c1608be1888d08fd0af5e867822b45b27e9b4
d.gnezdilov <gnezdiloveltech@gmail.com>
2021-09-24 15:48:49 +0300

    hgj

--- Package.swift
+++ Package.swift
@@ -3,17 +3,11 @@ import PackageDescription
 let package = Package(
     name: "FLEX",
     platforms: [
-        .macOS(.v10_10),
-        .iOS(.v9),
-        .tvOS(.v9),
-        .watchOS(.v2)
+        .iOS(.v9)
     ],
     products: [
         .library(name: "FLEX",
-                 targets: ["FLEX"]),
-        .library(name: "FLEX-Dynamic",
-                 type: .dynamic,
-                 targets: ["FLEX"]),
+                 targets: ["FLEX"])
     ],
     targets: [
         .target(name: "FLEX",
