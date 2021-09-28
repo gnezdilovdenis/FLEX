73a9dd8542fb83af1b20973c907bf067bbcc80ef
d.gnezdilov <gnezdiloveltech@gmail.com>
2021-09-28 10:48:46 +0300

    123

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         .binaryTarget(
             name: "FLEX",
             url: "https://github.com/gnezdilovdenis/FLEX/releases/download/999999/FLEX.zip",
-            checksum: "The checksum of the ZIP archive that contains the XCFramework."
+            checksum: "39f63a639c799426171c289198e66ff542702a0b69fd656f3fcdf0001a8b56bd055481998b2711809c1efdbdca3994f1a320bccfcd53b40f850122e5f03e2e28"
         )
     ]
 )
