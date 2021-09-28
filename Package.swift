9e83d013d20b6747e9102490b665bcad3e4f160b
d.gnezdilov <gnezdiloveltech@gmail.com>
2021-09-28 11:45:05 +0300

    123_test

--- Package.swift
+++ Package.swift
@@ -1,19 +1,12 @@
 // swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 let package = Package(
     name: "FLEX",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v11)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "FLEX",
-            targets: ["FLEX"])
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
+        .library( name: "FLEX", targets: ["FLEX"])
     ],
     targets: [
         .binaryTarget(
